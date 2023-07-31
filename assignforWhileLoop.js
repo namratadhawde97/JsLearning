047c496f37c7e95a546d5deea4e115675e77c5e7
namratadhawde97 <namratadhawde97@gmail.com>
2023-07-31 11:16:02 +0530

    assignment For and While Loop

---
+++ assignforWhileLoop.js
@@ -0,0 +1,87 @@
+console.log('------------------------------Assignment No:01 (For Loop) --------------');
+
+console.log('---------Step1--------------');
+for (let index = 5; index <=15; index++) {
+    console.log(index);   
+}
+
+console.log('---------Step2--------------');
+for (let index = 50; index >=40 ; index--) {
+    console.log(index);
+}
+
+console.log('---------Step3--------------');
+for (let index = 1; index <=30 ; index=index+2) {
+    console.log(index);
+}
+
+console.log('---------Step4--------------');
+for (let index = 0; index <=20 ; index=index+2) {
+    console.log(index);
+}
+
+console.log('---------Step5--------------');
+for (let index = 5; index <=50 ; index=index+5) {
+    console.log(index);
+}
+
+console.log('---------Step6--------------');
+for (let index = 10; index <=100 ; index=index+10) {
+    console.log(index);
+}
+
+console.log('---------Step7--------------');
+for (let index = 100; index >=10 ; index=index-10) {
+    console.log(index);
+}
+
+console.log('------------------------------Assignment No:02 (While Loop) --------------');
+
+console.log('---------Step1--------------');
+var i = 5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+
+console.log('---------Step2--------------');
+var i = 50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+
+console.log('---------Step3--------------');
+var i = 1;
+while (i<=30) {
+    console.log(i);
+    i=i+2;
+}
+
+console.log('---------Step4--------------');
+var i = 0;
+while (i<=20) {
+    console.log(i);
+    i=i+2;
+}
+
+console.log('---------Step5--------------');
+var i = 5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+}
+
+console.log('---------Step6--------------');
+var i = 10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+}
+
+console.log('---------Step7--------------');
+var i = 100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+}
