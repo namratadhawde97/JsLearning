30acf7a586310af4b726c6540be7e41f23a21be1
namratadhawde97 <namratadhawde97@gmail.com>
2023-08-09 21:43:18 +0530

    assignFactorial and local storage

---
+++ 11_assignFactorial.js
@@ -0,0 +1,47 @@
+
+function factorial(num){
+    if(num==1) {
+     return 1;
+    } else {
+    return num * factorial(num-1); 
+    } 
+ }
+ let five = factorial(5);
+ console.log(`Factorial of 5 is: ${five}`);
+ 
+ function factorial(num){
+     if(num==1) {
+      return 1;
+     } else {
+     return num * factorial(num-1); 
+     } 
+  }
+  let nine = factorial(9);
+  console.log(`Factorial of 9 is: ${nine}`);
+ 
+  function factorial(num){
+     if(num==1) {
+      return 1;
+     } else {
+     return num * factorial(num-1); 
+     } 
+  }
+  let eleven = factorial(11);
+  console.log(`Factorial of 11 is: ${eleven}`);
+ 
+  function factorial(num){
+     if(num==1) {
+      return 1;
+     } else {
+     return num * factorial(num-1); 
+     } 
+  }
+  let seven = factorial(7);
+  console.log(`Factorial of 7 is: ${seven}`);
+ 
+ 
+  console.log("-------------Assignment No=03---------------");
+ 
+ const arrayNums =[20,3,4,56,90,400,49];
+ arrayNums.push(55,66)
+ console.log(arrayNums);
